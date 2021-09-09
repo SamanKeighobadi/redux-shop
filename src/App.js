e115c96f2bf79ca4d3b44a66b5ff6a749dde1633
saman keighobadi <saman.keighobadi@gmail.com>
2021-09-09 16:12:18 +0430

    createa container component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-function App() {
-  return <div className="App"></div>;
-}
+import AppContainer from "./components/containers/AppContainer";
 
+const App = () => {
+  return (
+    <div>
+      <AppContainer />
+    </div>
+  )
+}
 export default App;
