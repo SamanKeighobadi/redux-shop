6207146e7edfee95fa6a7c5dfbfdcf43676c3113
saman keighobadi <saman.keighobadi@gmail.com>
2021-09-10 11:43:22 +0430

    install react router dom

--- src/components/Products/ProductCards.jsx
+++ src/components/Products/ProductCards.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import {setProdcuts} from '../../redux/actions/productActions'
 
 import ProductCard from "./ProductCard";
 
-const ProductCards = ({ products }) => {
-  return <div></div>;
+const ProductCards = () => {
+  const products = useSelector((state) => state.allProducts.products);
+    const dispatch = useDispatch()
+
+    const fetchProducts = async() => {
+        try {
+            const {data} = await axios.get('https://fakestoreapi.com/products')
+            dispatch(setProdcuts(data))
+        } catch (err) {
+            
+        }
+    }
+
+    useEffect(() =>{
+        fetchProducts()
+    },[])
+
+  return (
+    <div>
+      <ProductCard />
+      {products.map(product => (
+          <div>
+            {product.title}
+          </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProductCards;
