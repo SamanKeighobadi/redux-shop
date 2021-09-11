3643143d20799e665bb70683c9dfa2b426f1e430
saman keighobadi <saman.keighobadi@gmail.com>
2021-09-11 20:31:00 +0430

    register component ui component

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React from "react";
 // Material UI Component
-import {Te
-} from '@material-ui/core'
+import { TextField, Container, Typography, Button } from "@material-ui/core";
 
 const Register = () => {
-    return (
-        <div>
-         
-         
-        </div>
-    );
+  return (
+    <Container>
+      <Typography variant="h3" style={{ margin: "12px 0 " }} component="h3">
+        Login
+      </Typography>
+      <TextField
+        id="fullname"
+        label="Fullname"
+        type="text"
+        required
+        fullWidth
+        size="small"
+        variant="outlined"
+        helperText="Please enter you fullname"
+      />
+      <TextField
+        id="email"
+        label="Email"
+        type="email"
+        required
+        fullWidth
+        variant="outlined"
+        size="small"
+        helperText="Please enter you email"
+      />
+      <TextField
+        id="password"
+        type="password"
+        variant="outlined"
+        label="Password"
+        fullWidth
+        size="small"
+        helperText="Please enter your Password"
+      />
+      <Button type="submit" color="primary" variant="contained">
+        Submit
+      </Button>
+    </Container>
+  );
 };
 
 export default Register;
