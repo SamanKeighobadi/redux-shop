fe3348a4a0ce32f3032ec7e1a00590a5849d51b1
saman keighobadi <saman.keighobadi@gmail.com>
2021-09-11 20:00:33 +0430

    create login and register components

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// Material UI Component
+import {Typography} from '@material-ui/core'
+
+const Register = () => {
+    return (
+        <div>
+            <Typography variant={'h1'} >
+                Register
+            </Typography>
+        </div>
+    );
+};
+
+export default Register;
