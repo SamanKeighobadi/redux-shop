66d93b1c7519b0503976f91a2549f79a1b1c315d
saman keighobadi <saman.keighobadi@gmail.com>
2021-09-12 11:21:49 +0430

    set fomrik and validation for login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,12 +5,17 @@ import { Typography, Button, TextField, Container } from "@material-ui/core";
 import { Helmet } from "react-helmet";
 // Formik and form validation
 import { useFormik } from "formik";
+import { loginValidationShema } from "./loginValidationShcema";
 
 const Login = () => {
   const formik = useFormik({
     initialValues: {
       email: "",
       password: "",
+    },
+    validationSchema: { loginValidationShema },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
     },
   });
 
@@ -20,32 +25,40 @@ const Login = () => {
       <Helmet>
         <title>Login Page</title>
       </Helmet>
-      <Typography variant="h3" style={{ margin: "12px 0" }} component="h3">
-        Login
-      </Typography>
-      <TextField
-        type="email"
-        fullWidth
-        id="email"
-        label="Email"
-        size="small"
-        required
-        variant="outlined"
-        helperText="Please enter your email"
-      />
-      <TextField
-        type="password"
-        fullWidth
-        id="password"
-        label="Password"
-        required
-        size="small"
-        variant="outlined"
-        helperText="Please enter your password"
-      />
-      <Button type="submit" variant="contained" color="primary" size="small">
-        Submit
-      </Button>
+      <form onSubmit={formik.handleSubmit}>
+        <Typography variant="h3" style={{ margin: "12px 0" }} component="h3">
+          Login
+        </Typography>
+        <TextField
+          type="email"
+          fullWidth
+          id="email"
+          label="Email"
+          size="small"
+          required
+          variant="outlined"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          error={formik.touched.email && formik.errors.email}
+          helperText={formik.touched.email && formik.errors.email}
+        />
+        <TextField
+          type="password"
+          fullWidth
+          id="password"
+          label="Password"
+          required
+          size="small"
+          variant="outlined"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          error={formik.touched.password && formik.errors.password}
+          helperText={formik.touched.password && formik.errors.password}
+        />
+        <Button type="submit" variant="contained" color="primary" size="small">
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 };
