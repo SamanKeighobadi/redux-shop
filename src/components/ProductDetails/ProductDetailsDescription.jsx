e56ec984fc38160ee0f6652344d15bedc73c20d8
saman keighobadi <saman.keighobadi@gmail.com>
2021-09-13 12:18:37 +0430

    divided product details to two components

---
+++ src/components/ProductDetails/ProductDetailsDescription.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductDetailsDescription = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProductDetailsDescription;
