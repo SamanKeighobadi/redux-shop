5acef87bea88fa716e1336546a3a673491e5f11e
saman keighobadi <saman.keighobadi@gmail.com>
2021-10-03 23:23:12 +0330

    daily commit

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h1>Footer</h1>
+            <h1>saman.keighobadi@gmail.com</h1>
         </div>
     );
 };
